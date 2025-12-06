192be1c55ea8de05754c1acf81a1a1dba47aa851
Dlayk Mark <botrugnom7@gmail.com>
2025-12-06 15:02:28 +0100

    fix: Rimuovi la protezione delle rotte per la pagina Home

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,8 +16,7 @@ const routes = [
     {
         path: '/home',
         name: 'Home',
-        component: Home,
-        meta: { requiresAuth: true }
+        component: Home
     },
     {
         path: '/login',

d489859d0babe1d6e2a4d372181995e273a8dbb4
Marco Luigi Botrugno <marco@beforweb.net>
2025-12-11 12:51:28 +0100

    feat: Aumenta il limite di richieste per IP a 3000 nel middleware di rate limiting

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use(helmet()); // Set security headers
 // Rate limiting
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 1000, // Limit each IP to 1000 requests per windowMs
+    max: 3000, // Limit each IP to 3000 requests per windowMs
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });

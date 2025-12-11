fba02ded59aa46826aa26f8b2c8f4ce414d01028
Dlayk Mark <botrugnom7@gmail.com>
2025-12-11 21:46:41 +0100

    fix(vite): aggiungi configurazione di build per la gestione dei chunk

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,17 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/',
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['vue', 'vue-router', 'vuex', 'vue-i18n', 'axios'],
+          'ionic': ['@ionic/vue', '@ionic/vue-router', 'ionicons'],
+          'leaflet': ['leaflet'],
+          'socket': ['socket.io-client']
+        }
+      }
+    }
+  }
 })

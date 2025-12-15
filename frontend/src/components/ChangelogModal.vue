0458f4fe625517be62ce62e71bc47961a0259764
Dlayk Mark <botrugnom7@gmail.com>
2025-12-15 17:11:25 +0100

    feat(ChangelogModal): aggiunge colore di avviso al pulsante di donazione

--- frontend/src/components/ChangelogModal.vue
+++ frontend/src/components/ChangelogModal.vue
@@ -31,7 +31,7 @@
             </ion-item>
           </ion-list>
           <div class="modal-actions ion-padding-top">
-            <ion-button @click="openDonation">
+            <ion-button color="warning" @click="openDonation">
               <ion-icon :icon="beerOutline" slot="start"></ion-icon>
               {{ t("common.donate") }}
             </ion-button>
